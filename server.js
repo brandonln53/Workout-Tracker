fe538e24106ded827af7c035ca45f6b77c30ee6e
brandonln53 <brandonln@gmail.com>
2020-05-14 06:58:43 -0600

    added logger to server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(morgan("dev"));
+app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
