53853707f42e2867ed9d91c0b4d16066e2c078a2
brandonln53 <brandonln@gmail.com>
2020-05-20 12:05:01 -0600

    mongodb connection modifications

--- server.js
+++ server.js
@@ -13,10 +13,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+
+mongoose.connect(MONGODB_URI);
 
 // routes
 app.use(require("./routes/api.js"));
